a6f1cd4f2c601cded84b1aaaefc6e381d34b018f
Aju100 <ajutamang10@outlook.com>
2021-03-23 08:21:10 +0530

    configure:⚡️⚡️⚡️  models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class YourTask(models.Model):
+    title = models.CharField(max_length = 250)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
