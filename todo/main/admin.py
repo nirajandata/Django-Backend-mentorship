92a11ecd5194ee4798e013c93aef451503ffa983
root <root@localhost.localdomain>
2021-05-28 16:46:00 +0545

    CHORE:Admin Panel Customize

--- todo/main/admin.py
+++ todo/main/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import YourTask
 
-admin.site.register(YourTask)
+from .helpers import ExportCsvMixin
+is_superuser=True
+@admin.register(YourTask)
+class YourTaskAdmin(admin.ModelAdmin, ExportCsvMixin):
+    actions = ["export_as_csv","all_complete","all_not_complete"]
+    list_display=('title','created','completed')
+    list_per_page = 80
+    date_hierarchy = 'added_on'
+    readonly_fields = ["added_on"]
--- todo/users/admin.py
+++ todo/users/admin.py
@@ -2,15 +2,24 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
-
+import sys
+from pathlib import Path
 from todo.users.forms import UserChangeForm, UserCreationForm
+
+is_superuser=True
 
 User = get_user_model()
 
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent /"todo"
+main_dir=str(ROOT_DIR)+'/main/'
+sys.path.append(main_dir)
+from helpers import ExportCsvMixin 
 
 @admin.register(User)
-class UserAdmin(auth_admin.UserAdmin):
-
+class UserAdmin(auth_admin.UserAdmin,ExportCsvMixin):
+    admin.site.site_header = "TODO ADMIN PANEL"
+    admin.site.site_title = "WE LOVE OPENSOURCE"
+    admin.site.index_title = "INTERACTIVE TODO APP"
     form = UserChangeForm
     add_form = UserCreationForm
     fieldsets = (
@@ -32,3 +41,7 @@ class UserAdmin(auth_admin.UserAdmin):
     )
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
+    actions = ["export_as_csv"]
+    list_per_page = 60
+    date_hierarchy = 'added_on'
+    readonly_fields = ["added_on"]
