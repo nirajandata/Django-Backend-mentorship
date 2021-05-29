54cf6328fb9734d0fdca5129c2b4095585744ef1
root <root@localhost.localdomain>
2021-05-29 12:03:36 +0545

    CHORE:update admin panel

--- todo/main/admin.py
+++ todo/main/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from .models import YourTask
-
 from .helpers import ExportCsvMixin
+
 is_superuser=True
 @admin.register(YourTask)
 class YourTaskAdmin(admin.ModelAdmin, ExportCsvMixin):
     actions = ["export_as_csv","all_complete","all_not_complete"]
     list_display=('title','created','completed')
     list_per_page = 80
-    date_hierarchy = 'added_on'
-    readonly_fields = ["added_on"]
+
