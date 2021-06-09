55aa75ac9272bbaa503bb734b3b806f5c3e62616
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-06-09 04:52:12 +0000

    refracter code

--- config/settings/jazzmin_config.py
+++ config/settings/jazzmin_config.py
@@ -94,8 +94,8 @@ JAZZMIN_SETTINGS = {
     # override change forms on a per modeladmin basis
     "changeform_format_overrides": {
         "auth.user": "collapsible",
-        "auth.group": "vertical_tabs"
-        },
+        "auth.group": "vertical_tabs",
+    },
     # Add a language dropdown into the admin
     "language_chooser": True,
 }
