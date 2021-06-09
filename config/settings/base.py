eeb75536b37011caa9249143223b4f2a9d18fd0d
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-06-09 06:22:16 +0000

    code refracter

--- config/settings/base.py
+++ config/settings/base.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 import environ
 
-
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # todo/
 APPS_DIR = ROOT_DIR / "todo"
@@ -179,8 +178,6 @@ JAZZMIN_SETTINGS = {
     "hide_apps": [],
     # Hide these models when generating side menu (e.g auth.user)
     "hide_models": [],
-    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-    # for the full list of 5.13.0 free icon classes
     "icons": {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
